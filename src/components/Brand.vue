911fb92d16a7b41fe42d07e1324ec079328e96be
Hirunikh <hirunikh14@gmail.com>
2023-04-05 23:16:38 +0530

    brand

--- src/components/Brand.vue
+++ src/components/Brand.vue
@@ -1,54 +1,79 @@
 <template>
     <div class="form">
         <h2 style="text-align: center;">Brand</h2>
-        <b-form @create="onCreate" @update="onUpdate" @delete="onDelete" >
-            <b-form-group class="margins" id="input-group-1" label="Brand-Id:" label-for="input-1">
+        <b-form>
+            <b-form-group class="margins" id="input-group-1" label="Brand ID:" label-for="input-1">
                 <b-form-input id="input-1" v-model="form.id" placeholder="Autogenerate ID" ></b-form-input>
             </b-form-group>
-            <b-form-group class="margins" id="input-group-2" label="Brand-Name:" label-for="input-2">
+            <b-form-group class="margins" id="input-group-2" label="Brand Name:" label-for="input-2">
                 <b-form-input id="input-2" v-model="form.brandname" placeholder="Enter Brand Name" required></b-form-input>
             </b-form-group>
 
             <div class="buttonsAtBottom">
                 <b-button class="padding" type="create" variant="primary">Create</b-button>
                 <b-button class="padding" type="update" variant="secondary">Update</b-button>
-                <b-button class="padding" type="delete" variant="danger">Delete</b-button>
             </div>
 
         </b-form>
         <div>
-            <h5 style="text-align: center;">Brand Details</h5>   
-    <b-table class="margins" hover :items="items" :striped="striped" :bordered="bordered" :outlined="outlined"></b-table>
-  </div>
+            <h5 style="text-align: center;">Brand Details</h5>
+            <b-table class="margins" hover :items="items" :fields="fields" :striped="striped" :bordered="bordered" :outlined="outlined">
+                <template  v-slot:cell(select)="{ item }">
+                    <span><b-btn  @click="selectItem(item)" variant="outline-primary" >Select</b-btn></span>
+                </template>
+                <template v-slot:cell(delete)="{ item }">
+                    <span><b-btn variant="outline-success" @click="deleteItem(item)"  >Delete</b-btn></span>
+                </template>
+            </b-table>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
     export default {
         data() {
         return {
-            value: "",
             form: {
                 id: '',
                 brandname: ''
             },
-            items: [
-          { brandId: '', brandname: '' }
-        ],
+            items: [],
+        fields: ["brandId", "brandName", "select", "delete"],    
         striped: true,
         bordered: true,
         outlined: true
         }
     },
+    mounted() {
+        this.loadBrands();
+    },
     methods: {
+        async loadBrands() {
+            let response  = await axios.get("/bumblebee-apis/api/V1/Brand")
+            let allbrands = response.data;
+            let allItems = [];
+            allbrands.forEach(brand => {
+                allItems.push({
+                    brandId: brand.brandID,
+                    brandName: brand.brandName
+                })
+            })
+            this.items = allItems;
+        },
+        async deleteItem(item) {
+           await axios.delete("")
+           this.loadBrands();
+        },
+        selectItem(item) {
+            this.form.brandname = item.brandName;
+            this.form.id = item.brandId;
+        },
         onCreate() {
 
         },
         onUpdate() {
 
-        },
-        onDelete(){
-            
         }
 
     }   
@@ -63,6 +88,8 @@
     width: 50%;
     border: 2px solid grey;
     padding: 10px;
+    border-radius: 10px;
+    background-color: white;
 }
 
 .buttonsAtBottom {
