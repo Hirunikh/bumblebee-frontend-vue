68bceccf0c313cbfe0cdbd88fb8cf0f5ff24b065
Hirunikh <hirunikh14@gmail.com>
2023-04-05 23:16:45 +0530

    category

--- src/components/Category.vue
+++ src/components/Category.vue
@@ -1,59 +1,109 @@
 <template>
     <div class="form">
         <h2 style="text-align: center;">Category</h2>
-        <b-form @create="onCreate" @update="onUpdate" @delete="onDelete" >
-            <b-form-group class="margins" id="input-group-1" label="Category-Id:" label-for="input-1">
-                <b-form-input id="input-1" v-model="form.id" placeholder="Autogenerate ID" ></b-form-input>
+        <b-form>
+            <b-form-group class="margins" id="input-group-1" label="Category ID:" label-for="input-1">
+                <b-form-input id="input-1" v-model="form.id" placeholder="Autogenerate ID"></b-form-input>
             </b-form-group>
-            <b-form-group class="margins" id="input-group-2" label="Category-Name:" label-for="input-2">
-                <b-form-input id="input-2" v-model="form.categoryname" placeholder="Enter Category Name" required></b-form-input>
+            <b-form-group class="margins" id="input-group-2" label="Category Name:" label-for="input-2">
+                <b-form-input id="input-2" v-model="form.categoryname" placeholder="Enter Category Name"
+                    required></b-form-input>
             </b-form-group>
 
             <div class="buttonsAtBottom">
-                <b-button class="padding" type="create" variant="primary">Create</b-button>
-                <b-button class="padding" type="update" variant="secondary">Update</b-button>
-                <b-button class="padding" type="delete" variant="danger">Delete</b-button>
+                <b-button class="padding" type="button" v-on:click="onCreate" variant="primary">Create</b-button>
+                <b-button class="padding" type="button" v-on:click="onUpdate" variant="secondary">Update</b-button>
             </div>
 
         </b-form>
         <div>
-            <h5 style="text-align: center;">Category Details</h5>   
-    <b-table class="margins" hover :items="items" :striped="striped" :bordered="bordered" :outlined="outlined"></b-table>
-  </div>
+            <h5 style="text-align: center;">Category Details</h5>
+            <b-table class="margins" hover :items="items" :fields="fields" :striped="striped" :bordered="bordered"
+                :outlined="outlined">
+                <template v-slot:cell(select)="{ item }">
+                    <span><b-btn @click="selectItem(item)" variant="outline-primary">Select</b-btn></span>
+                </template>
+                <template v-slot:cell(delete)="{ item }">
+                    <span><b-btn variant="outline-success" @click="deleteItem(item)">Delete</b-btn></span>
+                </template>
+            </b-table>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        data() {
+import axios from 'axios'
+export default {
+    data() {
         return {
-            value: "",
             form: {
                 id: '',
                 categoryname: ''
             },
             items: [
-          { categoryId: '', categoryname: '' }
-        ],
-        striped: true,
-        bordered: true,
-        outlined: true
+            ],
+            fields: ["categoryId", "categoryName", "select", "delete"],
+            striped: true,
+            bordered: true,
+            outlined: true
         }
     },
+    mounted() {
+        this.loadCategories();
+    },
     methods: {
-        onCreate() {
-
+        async loadCategories() {
+            let response = await axios.get("/bumblebee-apis/api/V1/Category")
+            let allCategories = response.data;
+            let allItems = [];
+            allCategories.forEach(category => {
+                allItems.push({
+                    categoryId: category.categoryID,
+                    categoryName: category.categoryName
+                })
+            })
+            this.items = allItems;
         },
-        onUpdate() {
+        // async deleteItem(item) {
+        //     await axios.delete('/bumblebee-apis/api/V1/Category/${this.form.Id}')
+        //     .then(response => {
+        //          console.log(response);
+        //     });      
+        //    this.loadCategories();
+        // },
+        async deleteItem(item) {
+            await axios.delete(`/bumblebee-apis/api/V1/Category/${this.item.categoryID}`)
+                .then(response => {
+                    console.log(response);
+                    this.loadCategories();
 
+                });
         },
-        onDelete(){
-            
+        selectItem(item) {
+            this.form.categoryname = item.categoryName;
+            this.form.id = item.categoryId;
+        },
+        async onCreate() {
+            await axios.post('/bumblebee-apis/api/V1/Category')
+                .then(function (response) {
+                    console.log(response);
+                    this.loadCategories();
+
+                })
+        },
+        async onUpdate() {
+            await axios.put(`/bumblebee-apis/api/V1/Category/${this.form.id}`, {
+                params: {
+                    categoryName: this.form.categoryname
+                }
+            })
+                .then((response) => {
+                    console.log(response);
+                    this.loadCategories();
+                });
         }
-
-    }   
-
     }
+}
 
 </script>
 
@@ -63,6 +113,8 @@
     width: 50%;
     border: 2px solid grey;
     padding: 10px;
+    background-color: white;
+    border-radius: 10px;
 }
 
 .buttonsAtBottom {
